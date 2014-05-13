19216c6593a2ab10902f93029613aea3d8a04375
bgoujaud <baptiste.goujaud@ens-cachan.fr>
2014-05-13 10:56:05 +0200

    new regression optimization

--- optimize3.m
+++ optimize3.m
@@ -1,13 +1,12 @@
-% The function optimize2 returns the optimal values of sigma and
-% -log10(lambda), using the function moy, for the error L1, L2 or Linf according to i, testing moy np times with np different coefficients.
-% We can use this function writting: "optimize2(X, Y, nv, p, np, i)".
 
-function[answer] = optimize3(X, Y, nv, p, np)
 
-sigma = 3*rand(np,1)+1;
+function[answer] = optimize3(X, H, Y, nv, p, np)
+
+sigma = 4*rand(np,1)+2;
 ll = 6*rand(np,1)+3;
 for j = 1 : np
-    err(j,:) = moy(X, Y, 10^(-ll(j)), sigma(j), nv, p);
+    0.1*j
+    err(j,:) = moy2(X, H, Y, 10^(-ll(j)), sigma(j), nv, p);
 end
 
 answer = [sigma,ll,err];
