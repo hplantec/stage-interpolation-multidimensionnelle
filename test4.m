cee85e60ee7efaa3637feb3670fb79f7b39ef3d6
hplantec <hohodu37@hotmail.fr>
2014-06-02 16:02:26 +0200

    correction de test4.m

--- test4.m
+++ test4.m
@@ -5,27 +5,16 @@ Y = Y(:,4);
 X(ol63,:)=[];
 Y(ol63,:)=[];
 
-nv = 10;
+nv = 100;
 
 ll = 5*rand(100,1)+4;
 sigma = 4*rand(100,1)+0.5;
 c = (45*rand(100,1)+5) *10^-4;
-opt = [ll,sigma,c];
 
-for j = 1 : 3
-    p = 0.6+j/10;
+p = 0.8;
     
-    for i = 1 : 100
+    for i=1:100
         resultat(i,:) = asymetrique_cross(X, Y, 10^-ll(i) , sigma(i), c(i), nv, p)
-        i
     end
 
-    opt = [opt,resultat];
-    
-    figure(j)
-    scatter(resultat(:,1),resultat(:,2))
-    xlabel('erreur')
-    ylabel('proportion de points de au-dessus')
-    title(['Compromis à p=',num2str(p)])
-    
-end
+opt = [ll,sigma,c,resultat];
