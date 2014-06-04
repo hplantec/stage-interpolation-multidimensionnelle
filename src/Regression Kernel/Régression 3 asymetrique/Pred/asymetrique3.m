f543e9bc1635836a4f326f2392bf519888673c27
bgoujaud <baptiste.goujaud@ens-cachan.fr>
2014-06-04 17:07:46 +0200

    descente de gradient par newton

--- src/Regression Kernel/Régression 3 asymetrique/Pred/asymetrique3.m
+++ src/Regression Kernel/Régression 3 asymetrique/Pred/asymetrique3.m
@@ -6,7 +6,7 @@ Xtr1 = [Xtr,ones(size(Xtr,1),1)];
 A = (Xtr1'*Xtr1)\(Xtr1'*Ytr);
 Ztr = Ytr - Xtr1*A;
 
-B = asymetrique2(Xtr,Ztr,lambda,sigma,c);
+B = asymetrique(Xtr,Ztr,lambda,sigma,c);
 k=@(x,y) exp(-pdist2(x,y).^2/(2*sigma^2));
 K = k(Xte,Xtr);
 
