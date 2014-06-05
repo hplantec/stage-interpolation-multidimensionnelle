c63a10b31b3eee5fdbbf9d3564004460827df167
bgoujaud <baptiste.goujaud@ens-cachan.fr>
2014-06-05 04:47:02 +0200

    correction

--- src/Regression Kernel/Régression 5 symetrique if a+bh/optimize.m
+++ src/Regression Kernel/Régression 5 symetrique if a+bh/optimize.m
@@ -9,7 +9,7 @@ sigma3 = 5*rand(nr,1)+1;
 
 for i = 1 : nr
     erreur(i,:) = moy(X, H, Y, 10^-ll(i), sigma1(i), sigma2(i), sigma3(i), nv, p);
-    fprintf('%d%%\r', i/nr*100)
+    fprintf('%d%%\r', round(i/nr*100))
 end
 
 opt = [ll,sigma1,sigma2,sigma3,erreur];
