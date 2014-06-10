c77480e687c7d00b2b4fbc0c0cff78498aa01d2a
hplantec <hohodu37@hotmail.fr>
2014-06-10 19:57:24 +0200

    revalorisation des échelles des hyper-paramètres

--- src/Regression Kernel/Régression 5 symetrique if a+bh/optimizeif.m
+++ src/Regression Kernel/Régression 5 symetrique if a+bh/optimizeif.m
@@ -2,12 +2,12 @@
 
 function[opt] = optimizeif(X, H, Y, nv, nr, p)
 
-ll = 8*rand(nr,1)+3;
-sigma1 = 9*rand(nr,1)+1;
-sigma2 = 9*rand(nr,1)+1;
-sigma3 = 9*rand(nr,1)+1;
+ll = 3*rand(nr,1)+3.5;
+sigma1=0.3*rand(nr,1)-0.15;
+sigma2=100*rand(nr,1)-50;
+sigma3=100*rand(nr,1)-50;
 
-for i = 1 : nr
+for i = 1 : nr   
     erreur(i,:) = moyif(X, H, Y, 10^-ll(i), sigma1(i), sigma2(i), sigma3(i), nv, p);
     fprintf('%d%%\r', round(i/nr*100))
 end
--- src/Regression Kernel/Régression 5 symetrique if a+bh/testif.m
+++ src/Regression Kernel/Régression 5 symetrique if a+bh/testif.m
@@ -7,8 +7,26 @@ H = X(:,3);
 X = zscore(X(:,[2,4:8]));
 Y = Y(:,4);
 
-nv = 32;
-nr = 1000;
+nv = 4;
+nr = 500;
 p = 0.8;
 
 tic; opt = optimizeif(X, H, Y, nv, nr, p); toc
+
+% figure(1)
+% scatter(opt(:,1),log(opt(:,5)));
+% xlabel('-log(lambda)')
+% ylabel('log(erreur 1)')
+% 
+% for i=2:4
+% figure(i)
+% scatter(opt(:,i),log(opt(:,5)))
+% xlabel(['c ',num2str(i-1)])
+% ylabel('log(erreur 1)')
+% end
+
+% % ll=4.48;
+% % sigma1=-0.0348;
+% % sigma2=3.2281;
+% % sigma3=25.7547;
+% % erreur1=1.04%;
