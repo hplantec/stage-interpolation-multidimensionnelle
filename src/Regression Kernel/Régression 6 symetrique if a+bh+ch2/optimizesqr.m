a48d8bc7fe1987b5ce2dc93d81129951ab7b2029
hplantec <hohodu37@hotmail.fr>
2014-06-16 17:05:03 +0200

    affinage de la recherche des hyper paramètres

--- src/Regression Kernel/Régression 6 symetrique if a+bh+ch2/optimizesqr.m
+++ src/Regression Kernel/Régression 6 symetrique if a+bh+ch2/optimizesqr.m
@@ -2,11 +2,11 @@
 
 function[opt] = optimizesqr(X, H, Y, nv, nr, p)
 
-ll = 11*rand(nr,1)+1;
-sigma1 = 50*rand(nr,1)+1;
-sigma2 = 50*rand(nr,1)+1;
-sigma3 = 50*rand(nr,1)+1;
-sigma4 = 50*rand(nr,1)+1;
+ll = 6*rand(nr,1)+4;
+sigma1 = 10*rand(nr,1);
+sigma2 = 50*rand(nr,1);
+sigma3 = 50*rand(nr,1);
+sigma4 = 50*rand(nr,1);
 
 for i = 1 : nr
     erreur(i,:) = moysqr(X, H, Y, 10^-ll(i), sigma1(i), sigma2(i), sigma3(i), sigma4(i), nv, p);
